92c349586ce3ae31a6d1632c86984e78c4badfb2
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2019-12-27 10:57:27 +0800

    Update ApiDocTest.java

--- src/test/java/com/power/doc/ApiDocTest.java
+++ src/test/java/com/power/doc/ApiDocTest.java
@@ -12,6 +12,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.power.common.util.DateTimeUtil;
+import com.power.doc.builder.ApiDocBuilder;
+import com.power.doc.builder.HtmlApiDocBuilder;
+import com.power.doc.model.ApiConfig;
+import com.power.doc.model.SourceCodePath;
+
+import org.junit.Test;
+
 /**
  * Description:
  * ApiDoc测试
@@ -68,6 +76,59 @@ public class ApiDocTest {
 
         long start = System.currentTimeMillis();
 //        ApiDocBuilder.builderControllersApi(config);
+        long end = System.currentTimeMillis();
+        DateTimeUtil.printRunTime(end, start);
+    }
+    
+    
+    
+    private static final String SERVER_URL = "";
+    private static final String MAVEN_CODE_PATH = "";
+    private static final String MAVEN_DOC_MARK_PATH = "";
+    private static final String MAVEN_DOC_HTML_PATH = "";
+
+    @Test
+    public void testBuilderControllersApiMavenMarkDown() {
+        ApiConfig config = new ApiConfig();
+        config.setServerUrl(SERVER_URL);
+        config.setStrict(false);
+        config.setOutPath(MAVEN_DOC_MARK_PATH);
+        config.setPackageFilters("com.test");
+        config.setSrcCodePath(MAVEN_CODE_PATH);
+        //不指定SourceCodePaths默认加载代码为项目src/main/java下的,如果项目的某一些实体来自外部代码可以一起加载
+        config.setSourceCodePaths(
+            // SourceCodePath.path().setDesc("本项目代码").setPath("src/main/java")
+            //  SourceCodePath.path().setPath("E:\\Test\\Mybatis-PageHelper-master\\src\\main\\java"),
+            SourceCodePath.path().setDesc("加载项目外代码").setPath(config.getSrcCodePath())
+        );
+
+        long start = System.currentTimeMillis();
+        ApiDocBuilder.builderControllersApi(config);
+        long end = System.currentTimeMillis();
+        DateTimeUtil.printRunTime(end, start);
+    }
+
+    @Test
+    public void testBuilderControllersApiMavenHtml() {
+        ApiConfig config = new ApiConfig();
+        config.setServerUrl(SERVER_URL);
+        config.setStrict(false);
+        config.setOutPath(MAVEN_DOC_HTML_PATH);
+        config.setPackageFilters("com.test");
+
+        //设置用md5加密html文件名,不设置为true，html的文件名将直接为controller的名称
+        config.setMd5EncryptedHtmlName(false);
+        config.setSrcCodePath(MAVEN_CODE_PATH);
+
+        //不指定SourcePaths默认加载代码为项目src/main/java下的,如果项目的某一些实体来自外部代码可以一起加载
+        config.setSourceCodePaths(
+            // SourceCodePath.path().setDesc("本项目代码").setPath("src/main/java")
+            //  SourceCodePath.path().setPath("E:\\Test\\Mybatis-PageHelper-master\\src\\main\\java"),
+            SourceCodePath.path().setDesc("加载项目外代码").setPath(config.getSrcCodePath())
+        );
+
+        long start = System.currentTimeMillis();
+        HtmlApiDocBuilder.builderControllersApi(config);//此处使用HtmlApiDocBuilder，ApiDocBuilder提供markdown能力
         long end = System.currentTimeMillis();
         DateTimeUtil.printRunTime(end, start);
     }
